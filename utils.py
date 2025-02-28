5cc393ecba843b3644fa8fb830803bda129b690b
robertmeier99 <r.meier@student.tudelft.nl>
2025-02-28 17:33:35 +0100

    added extract_frame function to utils

--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ Useful functions for trajectory processing and cloudmetrics computation.
 """
 
 import numpy as np
+import xarray as xr
 
 def where_both(condition_1,condition_2):
     return np.where(np.where(condition_1,True,False)*np.where(condition_2,True,False))
+
+def extract_frame(img,extent):
+    w_extent, e_extent, s_extent, n_extent = extent
+
+    return img.where(
+            (img.lat >= s_extent)
+            & (img.lat <= n_extent)
+            & (img.lon >= w_extent)
+            & (img.lon <= e_extent),
+            drop=True,
+        )
